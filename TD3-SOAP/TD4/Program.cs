43b135bae6ef37c14f2de1c78ac7875837dba0b6
Antoine Huot-marchand <antoine.huot-marchand@etu.univ-cotedazur.fr>
2022-03-11 09:44:32 +0100

    TD3 Part SOAP

---
+++ TD3-SOAP/TD4/Program.cs
@@ -0,0 +1,18 @@
+﻿using ServiceReference1;
+
+namespace TD3SOAP
+{
+
+    internal class CallSOAP
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var client = new ServiceReference1.CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+            int a = 12;
+            int b = 8;
+            Console.WriteLine("Voici l'addition de " + a + " et " + b);
+            Console.WriteLine(await client.ChannelFactory.CreateChannel().AddAsync(a, b));
+        }
+    }
+}
